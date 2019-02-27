adcabffd42907ad78efe5df09792067c133f4a02
zen05 <377491521@qq.com>
2019-02-27 11:09:57 +0800

    change NotificationDetail.js can require data from service

--- App/components/NotificationDetail.js
+++ App/components/NotificationDetail.js
@@ -1,34 +1,39 @@
 import React,{Component} from 'react';
 import { Text, View, ScrollView } from 'react-native';
+import Global from '../utility/global'
 
 export default class NotificationDetail extends Component{
+  constructor(){
+    super()
+    this.state={
+      notification:{}
+    }
+  }
+  componentDidMount=()=>{
+    var id=1;
+    // id = this.props.navigation.getParam('Aid');
+    console.log(id);
+    url = Global.baseUrl + ':' + Global.port + '/article/detail?aid='+id//后端程序未写完
+    // console.log(url)
+    fetch(url).then(res=>res.json()).then(res=>{
+      // console.log(res);
+      this.setState({
+        notification:res
+      })
+      
+      // console.log('status:',this.state.notification)
+    })
+
+  }
   render() {
     return (
       <ScrollView >
-        <Text style={{fontWeight:'bold',fontSize:20}}>学院院长孙汉旭、党委书记谢苗峰发表2019年新年贺辞</Text>
-        <Text style={{fontSize:16}}>
-即将过去的2018年，恰逢我国改革开放40周年。中国特色社会主义进入新时代，民办高等教育进入蓬勃发展的新时期，学院也进入改革发展的新阶段。功崇惟志，业广惟勤，回望2018年，我们紧紧围绕建设有影响力的应用型大学的办学目标以及“立德树人”的根本任务，坚持内涵发展、特色发展、创新发展，一路执着守望大学精神与理想，只争朝夕地提升质量、专注创新，矢志不渝地滋兰树蕙、培育英才，意气风发地服务社会、投身发展。一年来，学院励精图治、亮点纷呈，办学水平不断攀升：
-
-2018年的世纪，不忘初心继续前进。学院党委深入学习贯彻党的十九大精神和习近平新时代中国特色社会主义思想，切实落实全国高校思想政治工作会议以及全国教育大会精神，全面落实党要管党、从严治党，真抓实干。各党支部、团支部开展各类活动50余次；编制《党支部工作手册》，签订《全面从严治党责任书》，基层党组织和全体党员的示范引领作用明显增强；围绕“基层党建质量年”的工作部署，深入推进党建与思政建设。
-
-2018年的世纪，追求卓越步履铿锵。学院紧紧围绕信息、互联网领域和相关发展方向，优化专业结构，整合和调整专业，启动自动化新专业招生。继续推进基于CDIO工程教育理念的专业综合改革，开展基于OBE理念的课程大纲修订。学院2018年的计划报到率98.59%，2018届毕业生就业率97.00%、签约率95.37%，位居同类学校前列。同时，学院获得新华网教育论坛“2018年度综合实力独立学院”称号。
-
-2018年的世纪，团结拼搏力争上游。学院申报的《能力导向集成资源探索与实践多元培养应用型人才之路》获得北京市高等教育教学成果奖二等奖；1名教师获得教育部在线教育研究中心“2018年度智慧教学之星”，1名教师获得第二届“CHINA中国”陶瓷艺术设计大展铜奖，1名教师获得中国物流学会教改教研课题评比一等奖，1名教师获得外研社“教学之星”大赛一等奖，2名教师获得“外教社杯”全国高校外语教学大赛北京赛区二等奖，1名辅导员获得全国民办高校辅导员工作高峰论坛“优秀辅导员”。学生参加国内外各类学科竞赛人次占在校生人数的25%，并获得国际奖项17项、国家级奖项6项、省部级奖项88项。
-
-2018年的世纪，勇攀高峰捷报频传。学院科研水平、国际合作、创新团队建设均实现突破。学院科研团队主导申报的“手机（移动终端）动漫国际标准”获得中国文化艺术政府奖第三届动漫奖唯一“最佳动漫技术奖”；学院牵头成立ITU-T.621移动终端动漫国际标准产业联盟；移动媒体与文化计算北京市重点实验室牵头承办首届中国文化计算大会；艺术与传媒学院联合申报的项目“《诗经》艺术的数字化传播推广平台建设”获得国家艺术基金资助。学院全年承担4项纵向课题、6项横向课题，科研经费400余万；在核心期刊发表论文、申请专利、编写教材著作10余项。学院进一步拓展与完善合作办学项目，先后与法国利摩日计算机工程师学院、英国德蒙福特大学、英国金斯顿大学签署校际合作协议，选拔多批学生前往国外进行学习和交流。创新创业工作进一步推开，成功举办第三届“互联网+”大学生创新创业大赛、“创青春”全国大学生创业大赛；以创新创业基地为平台，打造多维创新环境，一批创新团队走向成熟，创新工作室的成果陆续涌现。
-
-2018年的世纪，精神指引文化传承。落实“育人为本”的理念，强化思想育人，通过新生引航工程、雨燕团校、世纪讲坛、自塑工程等方式，学生思想政治工作进一步创新；强化实践育人，承接北京市社会建设专项资金购买社会组织服务项目“红烛行动——名师大课堂”“高校师生志愿者助力成长，与延庆区青少年共谱青春”，成为延庆区冬奥世园志愿者培训基地，师生志愿者投身于各种助学、助困、助残、养老、文明城市以及大型文化活动。
-
-2018年的世纪，温暖无限扬帆远航。正式成立关心下一代工作委员会；顺利召开第二届教职工代表大会第二次会议暨第四届工会会员代表大会，广泛听取教职工对学院发展的意见和建议；深入学习建立健全师德长效机制，积极搭建平台、加强教育引导，广泛开展“师德师风月”系列活动。
-
-新目标嵌入新蓝图，新征程赋予新使命。成绩来之不易，必须倍加珍惜；任务依然艰巨，务必同心戮力。世纪的点滴进步凝聚着每一位世纪人的心血和智慧，未来的跨越发展更离不开全体世纪人的坚持和参与。让我们携起手来，践行“明德 励志 笃学 致用”的校训精神，锐意改革、完善治理，自强不息、知行合一，按照学院“十三五”发展规划的既定目标，落实立德树人根本任务，大力实施人才优先战略，以提高本科教学质量为主题，以课堂教学和课程考核方式改革为重点，全面推进教育教学改革，进一步提升学院核心竞争力，为将学院建设成为具有影响力的应用型大学和特色鲜明的工程师学院不懈奋斗！
-
-最后，衷心祝愿广大师生、各级校友新年快乐、工作顺利，阖家安康、万事如意！
-</Text>
-        <Text style={{fontSize:16,alignSelf:'flex-end'}}>北京邮电大学世纪学院</Text>
-        <Text style={{fontSize:16,alignSelf:'flex-end'}}>院长：孙汉旭</Text>
-        <Text style={{fontSize:16,alignSelf:'flex-end'}}>党委书记：谢苗峰</Text>
-        <Text style={{fontSize:16,alignSelf:'flex-end'}}>2018年12月28日</Text>
+        <Text style={{fontWeight:'bold',fontSize:20}}>{this.state.notification.title}</Text>
+        <Text style={{fontSize:16}}>{this.state.notification.content}</Text>
+        <Text style={{fontSize:16,alignSelf:'flex-end'}}>{this.state.notification.Organization}</Text>
+        <Text style={{fontSize:16,alignSelf:'flex-end'}}>{this.state.notification.issuer1}</Text>
+        <Text style={{fontSize:16,alignSelf:'flex-end'}}>{this.state.notification.issuer2}</Text>
+        <Text style={{fontSize:16,alignSelf:'flex-end'}}>{Global.timefilter(this.state.notification.issue_data)}</Text>
       </ScrollView>
     );
   }
--- App/utility/global.js
+++ App/utility/global.js
@@ -5,4 +5,12 @@
 export default {
     baseUrl:'http://192.168.47.1',//服务器url
     port:'3000',//服务器端口
+    timefilter:(time)=>{//时间转换函数，将数据库中展示的数据格式转换为年月日的格式
+        var mydate = new Date(time);
+        var year = mydate.getFullYear()
+        var month = mydate.getMonth()+1
+        var day = mydate.getDate()
+        // console.log('global.js line13',year,month,day)
+        return `${year}年${month}月${day}日`
+    }
 } 
