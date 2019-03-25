c1fb502ea7e254c5197cb3ddeae0d0a74fd46492
zen05 <377491521@qq.com>
2019-03-25 19:42:30 +0800

    can rcound

--- App/components/NotificationDetail.js
+++ App/components/NotificationDetail.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import { Text, View, ScrollView, ToastAndroid } from 'react-native';
+import { Text, View, ScrollView, ToastAndroid, Image,FlatList } from 'react-native';
 import Global from '../utility/global'
 
 export default class NotificationDetail extends Component{
@@ -39,10 +39,10 @@ export default class NotificationDetail extends Component{
       if(result.code == 200){
         this.setState((prevState)=>{
           const {data} = result.data;
-          console.log('nDetail line 51 data:',data);
           return {
             pno:prevState.pno+1,
-            pageCount:4
+            pageCount:4,
+            data:data
           }
         })
       }else{
@@ -52,8 +52,17 @@ export default class NotificationDetail extends Component{
     
   }
   componentWillUnmount=()=>{
+    var rcount = parseInt(this.state.notification.rcount) + 1;
+    var url = `${Global.baseUrl}:${Global.port}/article/changeRcount?aid=${this.state.aid}&rcount=${rcount}`
+    fetch(url).then(res=>res.json()).then();
     this.setState({
-      notification:null
+      aid:null,
+      pagesize:null,
+      pageCount:null,
+      pno:null,
+      data:null,
+      notification:null,
+      isGetComment:false
     })
   }
   handlerPress=(aid)=>{
@@ -62,15 +71,98 @@ export default class NotificationDetail extends Component{
     this.props.navigation.navigate('Commont',{aid})
   }
 
+  showItem=(info)=>{
+    console.log("showItem has been called")
+    return (<View style={{flexDirection:'row',marginTop:10}}>
+    {/* 用户头像 */}
+    <Image 
+        style={{width:40,height:40,marginLeft:15}} 
+        source={require("../imgs/1.jpg")}
+        flex={1}></Image>
+    {/* 评论内容 */}
+    <View style={{marginLeft:8,marginRight:15}} flex={7}>
+      {/* 用户名和赞 */}
+      <View  style={{flexDirection:'row',justifyContent:'space-between'}}>
+        <Text style={{}}>{info.Item.user_name}</Text>
+        <View style={{flexDirection:'row'}}>
+          <Image 
+            source={require('../imgs/zan.png')}
+            style={{height:16,width:16}}></Image>
+          <Text>赞</Text>
+        </View>
+      </View>
+      {/* 评论内容 */}
+      <View>
+        <Text>{info.Item.content}</Text>
+      </View>
+    </View>
+  </View>)
+  }
+
   //渲染留言
   getCommont=()=>{
-    return (
-      <View style={{flex:1,marginTop:30,backgroundColor:'#f2f2f2',
-        borderTopColor:'#f2f2f2',borderTopWidth:1}}>
-        <Text style={{textAlign:'center',color:'#5e7192',height:50,fontSize:16,marginTop:20,justifyContent:'center'}} 
-          onPress={()=>this.handlerPress(this.state.notification.aid)}>写留言</Text>
-      </View>
-    )
+    
+    console.log(this.state)
+    if(this.state.data){
+      console.log(this.state.data)
+      return (
+      <View style={{backgroundColor:'#f2f2f2',
+        borderTopColor:'#f2f2f2',borderTopWidth:1,paddingBottom:30}}>
+      {/* 留言区头部 */}
+        <View style={{ flexDirection:'row',justifyContent:'space-between',marginTop:20}}>
+          <Text style={{marginLeft:15,fontSize:16}}>评论区</Text> 
+          <Text 
+            style={{marginRight:15,color:'#5e7192',fontSize:16}}
+            onPress={()=>this.handlerPress(this.state.notification.aid)}>
+            留言
+          </Text>
+        </View>
+      {/* 留言区 */}
+      {
+        this.state.data.map((value,index)=>{
+          // console.log("this is value---------------",value)
+          return (<View style={{flexDirection:'row',marginTop:10,marginBottom:20}} key={index}>
+            {/* 用户头像 */}
+            <Image 
+                style={{width:40,height:40,marginLeft:15}} 
+                source={require("../imgs/1.jpg")}
+                flex={1}></Image>
+            {/* 评论内容 */}
+            <View style={{marginLeft:8,marginRight:15}} flex={7}>
+              {/*/* 用户名和赞 */}
+              <View  style={{flexDirection:'row',justifyContent:'space-between'}}>
+                <Text style={{}}>{value.user_name}</Text>
+                <View style={{flexDirection:'row'}}>
+                  <Image 
+                    source={require('../imgs/zan.png')}
+                    style={{height:16,width:16}}></Image>
+                  <Text>赞</Text>
+                </View>
+              </View>
+              {/*/ 评论内容 */}
+              <View>
+                <Text>{value.content}</Text>
+              </View>
+            </View>
+          </View>)
+        })
+      }
+      {/* <FlatList 
+       data={this.state.data} 
+       extraData={this.state}
+       renderItem={this.showItem}>
+                 
+              </FlatList> */}
+      </View>);
+    }else{
+      return (
+        <View style={{flex:1,marginTop:30,backgroundColor:'#f2f2f2',
+          borderTopColor:'#f2f2f2',borderTopWidth:1}}>
+          <Text style={{textAlign:'center',color:'#5e7192',height:50,fontSize:16,marginTop:10,justifyContent:'center'}} 
+            onPress={()=>this.handlerPress(this.state.notification.aid)}>写留言</Text>
+        </View>
+      )
+    }
   }
   render() {
     return (
